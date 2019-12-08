07699efba842da9dcf285b4cab669ff1c1f799fb
bjzhang03 <bjzhang03@foxmail.com>
2019-12-08 13:22:57 +0800

    add medium 0093

---
+++ medium/0093/medium_0093.go
@@ -0,0 +1,65 @@
+package _093
+
+import (
+	"strconv"
+)
+
+// 将ip地址数据转化为string格式的数据
+func ipAddressToStr(nums []int) string {
+	result := ""
+	for i := 0; i < len(nums)-1; i++ {
+		result = result + strconv.Itoa(nums[i]) + "."
+	}
+	result = result + strconv.Itoa(nums[len(nums)-1])
+	return result
+}
+
+// 判断是否是ip地址的操作
+func isIpAddress(nums []int) bool {
+	for _, key := range nums {
+		if key > 255 || key < 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// 深度优先回溯找到所有的ip地址组合
+func dfs(result *[]string, tmp []int, index int, str string, usedSet *map[string]bool) {
+	if (len(tmp) == 4 && index == len(str)) {
+		if isIpAddress(tmp) {
+			ipStr := ipAddressToStr(tmp)
+			// 除了判断是否已经使用过以外,还要判断字符的长度是否正确
+			if ok := (*usedSet)[ipStr]; !ok && len(ipStr) == len(str)+3 {
+				*result = append(*result, ipStr)
+			}
+		}
+	} else if index > len(str) || (len(tmp) >= 4 && index != len(str)) {
+		return
+	} else {
+		for i := 1; i <= 3; i++ {
+			// 找出将要处理的数据
+			mystr := ""
+			if index+i <= len(str) {
+				mystr = str[index : index+i]
+			} else {
+				mystr = str[index:len(str)]
+			}
+
+			myint, err := strconv.Atoi(mystr)
+			if err != nil {
+				return
+			}
+			tmp = append(tmp, myint)
+			dfs(result, tmp, index+i, str, usedSet)
+			tmp = tmp[:len(tmp)-1]
+		}
+	}
+}
+
+func restoreIpAddresses(s string) []string {
+	result := []string{}
+	usedSet := make(map[string]bool)
+	dfs(&result, []int{}, 0, s, &usedSet)
+	return result
+}
---
+++ medium/0093/medium_0093_test.go
@@ -0,0 +1,25 @@
+package _093
+
+import (
+	"reflect"
+	"testing"
+)
+
+// success
+func TestRestoreIpAddresses(t *testing.T) {
+
+	var restoreIpAddressesTest = []struct {
+		in       string   // input
+		expected []string // expected result
+	}{
+		{"25525511135", []string{"255.255.11.135", "255.255.111.35"}},
+		{"010010", []string{"0.10.0.10", "0.100.1.0"}},
+	}
+
+	for _, riat := range restoreIpAddressesTest {
+		actual := restoreIpAddresses(riat.in)
+		if !reflect.DeepEqual(actual, riat.expected) {
+			t.Errorf("restoreIpAddressesTest(%s) = %s; expected %s", riat.in, actual, riat.expected)
+		}
+	}
+}
