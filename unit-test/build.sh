160debf54d9ed33b79e33cdb7cbcbe2c1de0b190
bjzhang <bjzhang1991@gmail.com>
2020-08-14 16:20:51 +0800

    add ci/cd
    Signed-off-by: bjzhang <bjzhang1991@gmail.com>

---
+++ unit-test/build.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: build.sh
+# Author: bjzhang03
+# mail: bjzhang03@foxmail.com
+# Created Time: 2020年08月14日 星期五 15时33分21秒
+#########################################################################
+#!/bin/bash
+
+set -e
+go list github.com/bjzhang03/go-leetcode/... | xargs -t go build -v
---
+++ unit-test/run.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: run.sh
+# Author: bjzhang03
+# mail: bjzhang03@foxmail.com
+# Created Time: 2020年08月14日 星期五 14时52分43秒
+#########################################################################
+#!/bin/bash
+
+set -e
+go list github.com/bjzhang03/go-leetcode/... | xargs -t -i go test -cover -race {}
