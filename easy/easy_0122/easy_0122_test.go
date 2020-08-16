806b3cd0028fdd3b877a754393e6a9e4207a8f43
bjzhang03 <bjzhang03@foxmail.com>
2020-08-16 11:09:59 +0800

    add easy_0122 unit test
    Signed-off-by: bjzhang03 <bjzhang03@foxmail.com>

---
+++ easy/easy_0122/easy_0122_test.go
@@ -0,0 +1,26 @@
+package easy_0122
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxProfit(t *testing.T) {
+	var mp = []struct {
+		prices   []int
+		expected int
+	}{
+		{[]int{7, 1, 5, 3, 6, 4}, 7},
+		{[]int{1, 2, 3, 4, 5}, 4},
+		{[]int{7, 6, 4, 3, 1}, 0},
+	}
+
+	for _, val := range mp {
+		actual := maxProfit(val.prices)
+
+		if actual != val.expected {
+			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(actual))
+		}
+	}
+
+}
