981a353f186cb87994fa4b5b551a2737645dd0a6
bjzhang03 <bjzhang03@foxmail.com>
2020-10-07 23:46:36 +0800

    add medium_0740
    Signed-off-by: bjzhang03 <bjzhang03@foxmail.com>

--- medium/medium_0740/medium_0740.go
+++ medium/medium_0740/medium_0740.go
@@ -1,35 +1,74 @@
 package medium_0740
 
+import (
+	"fmt"
+	"sort"
+)
+
 func deleteAndEarn(nums []int) int {
 	if len(nums) > 0 {
-		save := make(map[int]int)
+		// 先对数据进行排序操作
+		sort.Ints(nums)
+		// 先对数据进行存储操作,计算出每一个数据有多少个
+		save := make(map[int]int, 10000)
+		// 存储去重之后的数据
+		uniqnums := []int{}
 		for i := 0; i < len(nums); i++ {
-			if _, ok := save[nums[i]]; ok {
+			uniqnums = append(uniqnums, nums[i])
+			save[nums[i]] = 1
+			for i+1 < len(nums) && nums[i] == nums[i+1] {
+				i++
 				save[nums[i]]++
-			} else if !ok {
-				save[nums[i]] = 1
 			}
 		}
-
-		return solve(save, &map[string]int{})
+		return solve(uniqnums, &save, &map[string]int{})
 	}
 	return 0
 }
 
-func solve(mnums map[int]int, save *map[string]int) int {
-	result := 0
-	if len(mnums) > 0 {
-		for key, val := range mnums {
-			nmnums := make(map[int]int)
-			for nu, _ := range mnums {
-				if nu != key {
-					nmnums[nu] = mnums[key]
+// 使用一个calculated来存储计算过的数据,以便减少计算量
+func solve(uniqnums []int, save *map[int]int, calculated *map[string]int) int {
+	// 如果是已经计算过的结果则直接返回即可
+	if _, ok := (*calculated)[fmt.Sprint(uniqnums)]; ok {
+		return (*calculated)[fmt.Sprint(uniqnums)]
+	} else if !ok {
+		result := 0
+		if len(uniqnums) == 1 {
+			// 只有一个元素
+			result = (*save)[uniqnums[0]] * uniqnums[0]
+		} else if len(uniqnums) > 1 && uniqnums[0]+1 != uniqnums[1] {
+			// 第一个一定可以取到
+			result = (*save)[uniqnums[0]]*uniqnums[0] + solve(append([]int{}, uniqnums[1:]...), save, calculated)
+		} else if len(uniqnums) > 1 && uniqnums[0]+1 == uniqnums[1] {
+			// 第一个元素可能没有办法取到
+			if len(uniqnums) == 2 {
+				// 只有两个元素的情况
+				if (*save)[uniqnums[0]]*uniqnums[0] > (*save)[uniqnums[1]]*uniqnums[1] {
+					result = (*save)[uniqnums[0]] * uniqnums[0]
+				} else {
+					result = (*save)[uniqnums[1]] * uniqnums[1]
+				}
+			} else if len(uniqnums) > 2 {
+				// 元素多余两个的情况
+				if uniqnums[1]+1 != uniqnums[2] {
+					if (*save)[uniqnums[0]]*uniqnums[0] > (*save)[uniqnums[1]]*uniqnums[1] {
+						result = (*save)[uniqnums[0]]*uniqnums[0] + solve(append([]int{}, uniqnums[2:]...), save, calculated)
+					} else {
+						result = (*save)[uniqnums[1]]*uniqnums[1] + solve(append([]int{}, uniqnums[2:]...), save, calculated)
+					}
+				} else if uniqnums[1]+1 == uniqnums[2] {
+					selectone := (*save)[uniqnums[0]]*uniqnums[0] + solve(append([]int{}, uniqnums[2:]...), save, calculated)
+					selecttwo := (*save)[uniqnums[1]]*uniqnums[1] + solve(append([]int{}, uniqnums[3:]...), save, calculated)
+					if selectone > selecttwo {
+						result = selectone
+					} else {
+						result = selecttwo
+					}
 				}
 			}
-			if tres := solve(nmnums, save); result < key*val+tres {
-				result = key*val + tres
-			}
 		}
+		(*calculated)[fmt.Sprint(uniqnums)] = result
+		return result
 	}
-	return result
+	return 0
 }
