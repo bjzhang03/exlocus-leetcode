872683501145cf29262798d9958222fa275a92dd
bjzhang03 <bjzhang03@foxmail.com>
2020-10-07 20:17:33 +0800

    add easy_0217_test
    Signed-off-by: bjzhang03 <bjzhang03@foxmail.com>

---
+++ easy/easy_0217/easy_0217_test.go
@@ -0,0 +1,26 @@
+package easy_0217
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestContainsDuplicate(t *testing.T) {
+	var cases = []struct {
+		nums     []int
+		expected bool
+	}{
+		{[]int{1, 2, 3, 1}, true},
+		{[]int{1, 2, 3, 4}, false},
+		{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
+	}
+
+	for _, val := range cases {
+		actual := containsDuplicate(val.nums)
+
+		if actual != val.expected {
+			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(actual))
+		}
+	}
+
+}
