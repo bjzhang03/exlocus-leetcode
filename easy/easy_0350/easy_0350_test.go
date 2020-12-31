0cb0b723e8c60708157dc748fc47c0a29cd4d5cf
bjzhang03 <bjzhang03@foxmail.com>
2020-12-31 15:33:47 +0800

    add easy_0350_test

---
+++ easy/easy_0350/easy_0350_test.go
@@ -0,0 +1,26 @@
+package easy_0350
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestIntersect(t *testing.T) {
+	var cases = []struct {
+		nums1    []int
+		nums2    []int
+		expected []int
+	}{
+		{[]int{1, 2, 2, 1}, []int{2, 2}, []int{2, 2}},
+		{[]int{4, 9, 5}, []int{9, 4, 9, 8, 4}, []int{4, 9}},
+	}
+
+	for _, val := range cases {
+		actual := intersect(val.nums1, val.nums2)
+
+		if !reflect.DeepEqual(actual, val.expected) {
+			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(actual))
+		}
+	}
+}
