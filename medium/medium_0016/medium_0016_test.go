2a13798e02dbd5f788da3cd85fe30bba78583fe0
bjzhang03 <bjzhang03@foxmail.com>
2021-01-01 15:10:16 +0800

    add medium_0016_test

---
+++ medium/medium_0016/medium_0016_test.go
@@ -0,0 +1,26 @@
+package medium_0016
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestThreeSumClosest(t *testing.T) {
+	/*构建矩阵测试的结构*/
+	var cases = []struct {
+		nums     []int
+		target   int
+		expected int
+	}{
+		{[]int{-1, 2, 1, -4}, 1, 2},
+	}
+	/*执行矩阵测试*/
+	for _, val := range cases {
+		actual := threeSumClosest(val.nums, val.target)
+		/*验证结果*/
+		if !reflect.DeepEqual(actual, val.expected) {
+			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(actual))
+		}
+	}
+}
