6efdc3f84602b4cd9549f0ff44f82123329c96e4
bjzhang03 <bjzhang03@foxmail.com>
2021-01-03 12:52:57 +0800

    add medium_0082_test

--- medium/medium_0082/utils.go
+++ medium/medium_0082/utils.go
@@ -1,7 +1,18 @@
 package medium_0082
 
+import "strconv"
+
 //新建一个listnode的类型数据
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func (p *ListNode) String() string {
+	result := ""
+	for it := p; it != nil; it = it.Next {
+		result = result + strconv.Itoa(it.Val) + " "
+
+	}
+	return result
 }
