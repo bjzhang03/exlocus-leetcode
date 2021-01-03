ab32751fa7159311d0632c6376de21b7e16717b2
bjzhang03 <bjzhang03@foxmail.com>
2021-01-03 12:34:25 +0800

    add medium_0079_test

--- medium/medium_0079/medium_0079_test.go
+++ medium/medium_0079/medium_0079_test.go
@@ -1,12 +1,26 @@
 package medium_0079
 
-import "testing"
-
-type existStr struct {
-	board [][]byte
-	word  string
-}
+import (
+	"fmt"
+	"testing"
+)
 
 func TestExist(t *testing.T) {
-
+	/*构建矩阵测试的结构*/
+	var cases = []struct {
+		board    [][]byte
+		word     string
+		expected bool
+	}{
+		{[][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "SEE", true},
+		{[][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCB", false},
+	}
+	/*执行矩阵测试*/
+	for _, val := range cases {
+		actual := exist(val.board, val.word)
+		/*验证结果*/
+		if actual != val.expected {
+			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(actual))
+		}
+	}
 }
