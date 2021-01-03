156d776da4cfb51e58f6770227c17d5fafa453be
bjzhang03 <bjzhang03@foxmail.com>
2021-01-03 22:23:30 +0800

    update easy_0350_test

--- easy/easy_0350/easy_0350_test.go
+++ easy/easy_0350/easy_0350_test.go
@@ -19,7 +19,7 @@ func TestIntersect(t *testing.T) {
 
 	for _, val := range cases {
 		actual := intersect(val.nums1, val.nums2)
-		// 计算两个几个的交集,可以排序再比较
+		// 计算两个集合的交集,可以排序再比较
 		sort.Ints(actual)
 		if sort.Ints(val.expected); !reflect.DeepEqual(actual, val.expected) {
 			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(actual))
