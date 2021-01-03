eb6e05ace02c1287472e1e7d2540319aaf989249
bjzhang03 <bjzhang03@foxmail.com>
2021-01-03 12:00:54 +0800

    add medium_0075_test

--- medium/medium_0075/medium_0075_test.go
+++ medium/medium_0075/medium_0075_test.go
@@ -1,23 +1,27 @@
 package medium_0075
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestSortColors(t *testing.T) {
 
-	var sortColorsTest = []struct {
-		in       []int // input
+	var cases = []struct {
+		nums     []int // input
 		expected []int // expected result
 	}{
 		{[]int{2, 0, 2, 1, 1, 0}, []int{0, 0, 1, 1, 2, 2}},
+		{[]int{2, 0, 1}, []int{0, 1, 2}},
+		{[]int{0}, []int{0}},
+		{[]int{1}, []int{1}},
 	}
 
-	for _, sct := range sortColorsTest {
-		sortColors(sct.in)
-		if !reflect.DeepEqual(sct.in, sct.expected) {
-			t.Errorf("sortColors(%d) = %d; expected %d", sct.in, sct.in, sct.expected)
+	for _, val := range cases {
+		sortColors(val.nums)
+		if !reflect.DeepEqual(val.nums, val.expected) {
+			t.Errorf("Test Failed! expected := %s, actual := %s", fmt.Sprint(val.expected), fmt.Sprint(val.nums))
 		}
 	}
 
